6368349d38211b5840fc12fcda03d1c2b6f695e6
Clark B. Wierda <cbwierda@gmail.com>
2015-08-31 22:23:28 -0400

    Improved documentation
    Many comments added to the code.
    Changes made to code and Readme to be more consistent

--- Readme.md
+++ Readme.md
@@ -1,9 +1,9 @@
 # ipscan - scan IP range and report IP, round-trip time (rtt), and DNS name for active devices
 
-This is based on [mping](https://github.com/mhusmann/mping) by [mhusmann](https://github.com/mhusmann).
-It has since been significantly modified as I learn more about Go and its concurrency patterns.
+This program is based on [mping](https://github.com/mhusmann/mping) by [mhusmann](https://github.com/mhusmann).
+It has been almost entirely re-written as I have learned more about Go and its concurrency patterns.
 
-Range is currently hard-coded as a constant at the start of the source.
+Range endpoints are currently hard-coded as constants near the top of the source.
 
 ## To Do
 
--- ipscan.go
+++ ipscan.go
@@ -1,5 +1,7 @@
-// Scans defined IP range and returns list of active devices, rtt, and DNS names
-// Started life based on mping (https://github.com/mhusmann/mping)
+// Scans defined IP range and returns list of active devices, rtt, and DNS names.
+// Started life based on mping (https://github.com/mhusmann/mping).
+// Currently dependent on the commands 'fping' and 'host' and a Bourne-ish shell environment.
+// Logging can be eliminated by redirecting stderr to /dev/null
 package main
 
 import (
@@ -12,10 +14,16 @@ import (
   "strings"
 )
 
+// Change constants to determine the range to be scanned.
+const startIp = "192.168.0.1"
+const endIp = "192.168.0.254"
+
 type Data struct {
   PingResult string
   HostResult string
 }
+
+// Functions and types needed to support sorting the results
 
 func (d Data) Octets() []int {
   parts := strings.SplitN(d.PingResult, " ", 2)
@@ -43,21 +51,23 @@ func (device ByIP) Less(i, j int) bool {
         return false
     }
   }
+  // This function should have returned by now as there shoudn't be duplicate IPs
+  // This final return is correct for the case where duplicate IPs are compared
+  // i < j is false for the case i == j
   return false
 }
-
-const startIp = "192.168.0.1"
-const endIp = "192.168.0.254"
 
 func main() {
   log.Println(": Program started")
 
+  // Configure an exec call to run fping in a subprocess and sent the output to a pipe
   cmd := exec.Command("fping", "-gae", startIp, endIp + " 2>/dev/null")
   cmdReader, err := cmd.StdoutPipe()
   if err != nil {
     log.Fatal("Error creating StdoutPipe for Cmd", err)
   }
   
+  // Create a goroutine to read the output from the pipe
   ips := make([]Data, 0)
   scanner := bufio.NewScanner(cmdReader)
   go func() {
@@ -68,12 +78,14 @@ func main() {
     }
   }()
   
+  // Start the subprocess prepared above
   err = cmd.Start()
   if err != nil {
     log.Fatal("Error starting Cmd", err)
   }
   log.Println(": Scan started")
   
+  // Wait for the subprocess to exit
   err = cmd.Wait()
   if err != nil && err.Error() != "exit status 1" {
     log.Fatal("Error waiting for Cmd", err)
@@ -84,6 +96,7 @@ func main() {
   fmt.Printf("%d devices found\n", len(ips))
   fmt.Println()
 
+  // Query DNS for the name of each device found by the ping scan
   var ipAndTime []string
   var out1 []byte
   var out2 []string
@@ -94,11 +107,11 @@ func main() {
     // extract hostname from result
     out2 = strings.SplitAfterN(string(out1), "pointer ", 2)
     switch len(out2) {
-    case 0:
+    case 0: // probably another error condition as this would be an empty slice
       hostname = "<undefined>"
-    case 1:
+    case 1: // usually indicates an error condition of some kind
       hostname = out2[0]
-    case 2:
+    case 2: // use everything after the anchor string which should be the name returned by DNS
       hostname = out2[1]
     default:
       log.Fatalf("Logic Error-> ip: %s result: %s", ipAndTime[0], string(out1))
