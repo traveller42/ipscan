03f908603c89f2762d5576ac355b165930af216c
Clark B. Wierda <cbwierda@gmail.com>
2016-01-21 23:19:29 -0500

    Replace shell out for host lookup with Go method
    Replace Unix-specific shell command with native Go lookup.
    Adjust related code to use the error value and, possibly multiple, hosts.

--- ipscan.go
+++ ipscan.go
@@ -8,6 +8,7 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"net"
 	"os/exec"
 	"sort"
 	"strconv"
@@ -98,23 +99,15 @@ func main() {
 
 	// Query DNS for the name of each device found by the ping scan
 	var ipAndTime []string
-	var out1 []byte
-	var out2 []string
+	var hosts []string
 	var hostname string
 	for index, ip := range ips {
 		ipAndTime = strings.SplitN(ip.PingResult, " ", 2)
-		out1, _ = exec.Command("host", ipAndTime[0]).Output()
-		// extract hostname from result
-		out2 = strings.SplitAfterN(string(out1), "pointer ", 2)
-		switch len(out2) {
-		case 0: // probably another error condition as this would be an empty slice
-			hostname = "<undefined>"
-		case 1: // usually indicates an error condition of some kind
-			hostname = out2[0]
-		case 2: // use everything after the anchor string which should be the name returned by DNS
-			hostname = out2[1]
-		default:
-			log.Fatalf("Logic Error-> ip: %s result: %s", ipAndTime[0], string(out1))
+		hosts, err = net.LookupAddr(ipAndTime[0])
+		if err != nil {
+			hostname = "Error: " + err.Error()
+		} else {
+			hostname = strings.Join(hosts, ", ")
 		}
 		ips[index].HostResult = hostname
 	}
@@ -124,7 +117,7 @@ func main() {
 	log.Println(": Sort complete")
 
 	for _, ip := range ips {
-		fmt.Printf("%-25s --> %s", ip.PingResult, ip.HostResult)
+		fmt.Printf("%-25s --> %s\n", ip.PingResult, ip.HostResult)
 	}
 
 	fmt.Println()
